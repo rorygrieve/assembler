b7376b55cce764dc27e4c71c02c94b72b0add3ec
Rory Grieve <rhrg500@gmail.com>
2018-08-10 15:09:16 +0100

    @wip spec for translating files w/o symbols
    This feature test will be used to drive development.

---
+++ spec/features/translate_file_spec.rb
@@ -0,0 +1,10 @@
+RSpec.describe "Translate files" do
+  context "a file without symbols is loaded", wip: true  do
+    it "is translated into machine code" do
+      main = Main.new(parser: Parser.new, translator: Translator.new)
+      main.call("spec/fixtures/Add.asm")
+
+      expect(IO.read("lib/output/Add.hack")).to eq(IO.read("spec/fixtures/Add.hack"))
+    end
+  end
+end
