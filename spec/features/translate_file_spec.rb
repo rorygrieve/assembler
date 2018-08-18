f361bc6035fc61c01b36e10a484100d03a53705e
rory <rhrg500@gmail.com>
2018-08-18 15:19:08 +0100

    @wip spec for code w/o symbols with jump commands

--- spec/features/translate_file_spec.rb
+++ spec/features/translate_file_spec.rb
@@ -4,11 +4,22 @@ require "translator"
 
 RSpec.describe "Translate files" do
   context "a file without symbols is loaded"  do
-    it "is translated into machine code" do
-      main = Main.new(parser: Parser.new, translator: Translator.new)
-      main.call("spec/fixtures/Add.asm")
+    context "without jump commands" do
+      it "is translated into machine code" do
+        main = Main.new(parser: Parser.new, translator: Translator.new)
+        main.call("spec/fixtures/Add.asm")
 
-      expect(IO.read("lib/output/Add.hack")).to eq(IO.read("spec/fixtures/Add.hack"))
+        expect(IO.read("lib/output/Add.hack")).to eq(IO.read("spec/fixtures/Add.hack"))
+      end
+    end
+
+    context "with jump commands" do
+      it "is translated into machine code", wip: true do
+        main = Main.new(parser: Parser.new, translator: Translator.new)
+        main.call("spec/fixtures/MaxL.asm")
+
+        expect(IO.read("lib/output/MaxL.hack")).to eq(IO.read("spec/fixtures/MaxL.hack"))
+      end
     end
 
     after do
